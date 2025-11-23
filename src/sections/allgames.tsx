c8d5c597d4261d669d3999922a39a3da1919fcc8
dagles27 <vimode@gmx.de>
2025-11-23 18:56:54 +0100

    allgames.tsx erstellen

---
+++ src/sections/allgames.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function AllGames() {
+  return (
+    <div style={{ padding: "80px 20px", color: "white" }}>
+      <h1 style={{ textAlign: "center" }}>🎮 All Games</h1>
+      <p style={{ textAlign: "center", opacity: 0.8 }}>
+        Here you can see all available games.
+      </p>
+
+      {/* später fügen wir hier die Spiele ein */}
+    </div>
+  )
+}
